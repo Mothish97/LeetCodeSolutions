1787cb026dba0accd89dee728adb72cf24d243a9
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-01-10 01:40:15 -0600

    Time: 367 ms (32.76%) | Memory: 77.7 MB (5.06%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) 
+    {
+        if(nums.Length ==0) return 0;
+        var sortedSet = new SortedSet<int>();
+        foreach(var n in nums)
+        {
+            sortedSet.Add(n);
+        }
+        var maxCt =1;
+        var ct = 1;
+        var sortList = sortedSet.ToList();
+        Console.WriteLine(string.Join(", ", sortList));
+        for(int i=0;i<sortList.Count-1;i++)
+        {
+            var diff = Math.Abs(sortList[i+1] - sortList[i]);
+            if(diff==1 )
+            {
+                ct++;
+            }
+            else{
+                ct=1;
+            }
+            maxCt = Math.Max(ct,maxCt);
+        }
+        return maxCt;
+    }
+}
