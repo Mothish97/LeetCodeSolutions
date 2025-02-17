c171c86ebc1da8b6c78994ab4ceb03ed6f357e0c
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-02-17 16:15:28 -0600

    Time: 10 ms (7.41%) | Memory: 70.3 MB (7.41%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cs
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cs
@@ -1,30 +1,11 @@
 public class Solution {
     public long MinimumHealth(int[] damage, int armor) 
     {
-        int maxVal = damage.Max();
+        long maxVal = damage.Max(); 
+        long totalDamage = damage.Sum(d => (long)d);
+        long damageReduced = Math.Min(maxVal, armor);
         
-        //
-        if(maxVal< armor)
-        {
-            int index = Array.IndexOf(damage, maxVal);
-            if (index != -1) // Ensure index is valid
-            {
-                damage[index] = 0;
-            }
-            long totalDamage = damage.Select(d => (long)d).Sum();
-            return totalDamage +1;
-        }
-        else
-        {
-            int index = Array.IndexOf(damage, maxVal);
-            if (index != -1) // Ensure index is valid
-            {
-                damage[index] = 0;
-            } 
-            long diff =(long) maxVal -(long) armor;
-            long totalDamage = damage.Select(d => (long)d).Sum();
-            return totalDamage +diff +1;
-        }
+        return totalDamage - damageReduced + 1;
         
     }
 }
