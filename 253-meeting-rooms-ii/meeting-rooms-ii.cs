957434a0d02bbb9812ddcff95ced42e764d75f6b
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-03-12 00:15:48 -0500

    Time: 8 ms (57.14%) | Memory: 45.6 MB (35.28%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MinMeetingRooms(int[][] intervals) {
+        Array.Sort(intervals,(a,b)=> 
+        a[0]== b[0] ? a[1].CompareTo(b[1]) :a[0].CompareTo(b[0]));
+        var pq = new PriorityQueue<int,int>();
+        var room = pq.Count;
+        foreach(var itr in intervals)
+        {
+            if(pq.Count ==0)
+            {
+                pq.Enqueue(itr[1],itr[1]);
+            }
+            else
+            {
+                var minR=  pq.Dequeue();
+                if(minR > itr[0])
+                {
+                    pq.Enqueue(minR,minR);
+                }
+       
+                pq.Enqueue(itr[1],itr[1]);
+            }
+
+            room = Math.Max(room,pq.Count);
+        }
+
+
+        return room;
+    }
+}
