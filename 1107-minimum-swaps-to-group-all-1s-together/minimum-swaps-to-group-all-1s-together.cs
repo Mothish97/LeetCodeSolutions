6a192d048a79d74d38660a9a47d582b2ff3be7de
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-03-26 01:10:35 -0500

    Time: 16 ms (8.70%) | Memory: 54.5 MB (18.01%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.cs
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.cs
@@ -6,11 +6,12 @@ public class Solution {
         var count = data.Skip(0).Take(slid).Count(x=> x==0);
         var result = count;
         
-        for(int i =0;i<=data.Length-slid-1;i++)
+        for (int i = slid; i < data.Length; i++)
         {
-            if(data[i] == 0) count--;
-            if(data[i+slid]==0) count++;
-            result = Math.Min(count,result);
+            if (data[i - slid] == 0) count--; // element leaving window
+            if (data[i] == 0) count++;        // element entering window
+
+            result = Math.Min(result, count);
         }
         return result;
     }
