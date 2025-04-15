bbd27de0766f7bfdc2b2da52b20c2c412c184ae6
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-04-15 03:55:18 -0500

    Time: 79 ms (0.00%) | Memory: 57.7 MB (0.00%) - LeetSync

---
+++ 756-pour-water/pour-water.cs
@@ -0,0 +1,56 @@
+public class Solution {
+
+    public void fillWater(int k , int [] heights)
+    {
+        var min= k;
+        for(int i = k; i>=0;i--)
+        {
+            if(heights[i]<heights[min])
+            {
+                min = i;
+            }
+            if(heights[i]> heights[k])
+            {
+                break;
+            }
+            if(heights[min]<heights[i])
+            {
+                break;
+            }
+        }
+        if(min!= k)
+        {
+            heights[min]++;
+            return;;
+        }
+        for(int i =k; i< heights.Length;i++)
+        {
+            if(heights[i]<heights[min])
+            {
+                min = i;
+            }
+            if(heights[i]> heights[k])
+            {
+                break;
+            }
+            if(heights[min]<heights[i])
+            {
+                break;
+            }
+        }
+        heights[min]++;
+    }
+    public int[] PourWater(int[] heights, int volume, int k)
+    {
+        var cur  = 0;
+        Console.WriteLine(string.Join(" ", heights));
+        while(cur<volume)
+        {
+            fillWater( k , heights);
+            cur++;
+            Console.WriteLine(string.Join(" ", heights));
+        }
+        return heights;
+        
+    }
+}
