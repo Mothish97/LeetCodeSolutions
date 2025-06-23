d1d46d0e96a8be67bc9f063d5547b7ce46a2cd21
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-06-23 00:32:55 -0500

    Time: 47 ms (31.95%) | Memory: 51.3 MB (12.21%) - LeetSync

---
+++ 322-coin-change/coin-change.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CoinChange(int[] coins, int amount) 
+    {
+        var dp = new int[amount +1];
+        for(int i =1;i<=amount;i++)
+        {
+            dp[i] = amount+1;
+            foreach(var c in coins)
+            {
+                if(i-c >= 0)
+                {
+                    dp[i] = Math.Min(dp[i], 1+ dp[i-c]);
+                }
+            }
+
+        }
+        Console.WriteLine(String.Join(",",dp));
+        if(dp[amount] == amount +1) return -1;
+        return dp[amount];
+        
+    }
+}
