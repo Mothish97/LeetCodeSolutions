c531562b5e6a20673008ddaee4e747acd60f158a
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-06-26 21:17:16 -0500

    Time: 18 ms (80.15%) | Memory: 66.6 MB (69.22%) - LeetSync

--- 49-group-anagrams/group-anagrams.cs
+++ 49-group-anagrams/group-anagrams.cs
@@ -1,25 +1,35 @@
 public class Solution {
-    public IList<IList<string>> GroupAnagrams(string[] strs)
-    {
-        var res = new List<IList<string>>();
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {       
         var dct = new Dictionary<string,List<string>>();
-
-        foreach(var st in strs)
+        var sorted = new List<string>();
+        var res = new List<IList<string>>();
+        foreach(var s in strs)
         {
-            string sorted = new string(st.OrderBy(c => c).ToArray());
-            if(dct.ContainsKey(sorted))
+            var c = s.ToCharArray();
+            Array.Sort(c);
+            sorted.Add(new string(c));
+        }
+        for(int i=0; i < sorted.Count;i++)
+        {
+            if(dct.ContainsKey(sorted[i]))
             {
-                dct[sorted].Add(st);
+                dct[sorted[i]].Add(strs[i]);
             }
             else
             {
-                dct.Add(sorted, new List<string>{st});
+                dct.Add(sorted[i],new List<string>(){strs[i]});
             }
         }
-        foreach(var d in dct)
+        foreach(var d in dct.Values)
         {
-            res.Add(d.Value);
+            res.Add(d);
         }
+        
+
+ 
         return res;
+
     }
+
 }
