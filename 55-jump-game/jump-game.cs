368493176e6a4d57f9d945b683c6f8f8740ee19d
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-06-28 00:27:12 -0500

    Time: 2 ms (63.16%) | Memory: 61.4 MB (23.53%) - LeetSync

--- 55-jump-game/jump-game.cs
+++ 55-jump-game/jump-game.cs
@@ -4,6 +4,7 @@ public class Solution {
         for(int i =0 ; i < nums.Length;i++)
         {
             if(i>maxReach) return false;
+
             maxReach = Math.Max(maxReach, i+nums[i]);
             if(maxReach>nums.Length-1) return true;
         }
