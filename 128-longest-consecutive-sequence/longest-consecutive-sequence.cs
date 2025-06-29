396c5dc379e62c2c654d94b883d936b499194d4e
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-06-28 22:07:05 -0500

    Time: 373 ms (5.02%) | Memory: 93.1 MB (5.07%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -1,27 +1,63 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) 
     {
-        if (nums.Length == 0) return 0;
+        if(nums.Count() == 1 ) return 1;
+        if(nums.Count() == 0) return 0;
 
-        var numSet = new HashSet<int>(nums);
-        var maxSum = 1;
-        
-        foreach(var n in numSet)
+        var dct = new HashSet<int>();
+        var hash = new HashSet<int>();
+        var count = 0;
+        var num = new HashSet<int>();
+        foreach(var n in nums)
         {
-            if(!numSet.Contains(n-1))
-            {
-                var curNum = n;
-                var curSeq = 1;
-                while(numSet.Contains(curNum+1))
-                {
-                    curSeq++;
-                    curNum++;
-                }
-                maxSum = Math.Max(curSeq,maxSum);
+            num.Add(n);
+        }
 
+        ///8  10  0  2 3 5 6 8 4  5
+        foreach(var val in num)
+        {
+            var prev = val-1;
+            var next = val+1;
+            dct.Add(prev);
+            dct.Add(next);
+        } 
+        foreach(var val in num)
+        {
+            if(dct.Contains(val) && !hash.Contains(val))
+            {
+                count++;
+                hash.Add(val);
             }
         }
-        return maxSum;
-
+        var pq = new PriorityQueue<int,int>();
+        foreach(var val in hash)
+        {
+            pq.Enqueue(val, val);
+        }
+    
+        if(count == 0)
+        {
+            return 1;
+        }
+        var res = 0;
+        var resar = new List<int>();
+        while(pq.Count>0)
+        {
+            resar.Add(pq.Dequeue());
+        }
+        count =0;
+        for(int i =1; i<resar.Count;i++)
+        {
+            if(Math.Abs(resar[i] - resar[i-1]) == 1)
+            {
+               count++; 
+            }
+            else{
+                count=0;
+            }
+            res = Math.Max(res,count);
+        }
+        //Console.WriteLine(hash.Count());
+        return res+1;
     }
 }
