89d1e5d544e4dbd27af3b2d48568cd1ba2dc9b84
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-06-29 18:32:58 -0500

    Time: 25 ms (11.01%) | Memory: 53.8 MB (29.52%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cs
@@ -0,0 +1,58 @@
+public class Solution {
+    public int ShipWithinDays(int[] weights, int days) 
+    {
+        var left = weights.Max();
+        var right = weights.Sum();
+        var res = weights.Sum();
+        while(left<=right)
+        {
+            var mid = left + (right - left) / 2; 
+            Console.WriteLine(mid);
+            if(possible(mid,weights,days))
+            {
+                right = mid-1;
+                res = Math.Min(res,mid);
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+        //Console.WriteLine(possible(5,weights,days));
+        //return 0;
+        
+    }
+
+    public bool possible(int value, int[] w,int days)
+    {
+        var right =0;
+        var curTotal =0;
+        var daysTaken = 0;
+        while(right<w.Length)
+        {
+            if(daysTaken ==  days ) return false;
+            curTotal = curTotal + w[right];
+            
+            if(curTotal > value)
+            {
+               // Console.WriteLine(curTotal- w[right] );
+                daysTaken++;
+                //Console.WriteLine($"Day : {daysTaken}");
+
+                if(daysTaken> days)
+                {
+                    return false;
+                }
+                curTotal = 0;
+                continue;
+            }
+            right++;
+            if(daysTaken> days)
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
