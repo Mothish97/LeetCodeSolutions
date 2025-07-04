849821d1b2fdb2b4e0fd50040dad3d2accc45413
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-07-04 02:12:32 -0400

    Time: 6 ms (10.75%) | Memory: 42.5 MB (8.60%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int MincostTickets(int[] days, int[] costs)
+    {
+     //[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]  
+     //[0,2,2,2,4,4,6,7,9,0,0, 0, 0, 0, 0, 0,0,0] 
+//                  (8vs7)
+
+    var dp = new int[days[days.Count()-1]+1];
+    var pass = new int[3]{1,7,30};
+
+    for(int n =1; n<dp.Count(); n++)
+    {
+        if(!days.Contains(n))
+        {
+            dp[n] = dp[n-1];
+            continue;
+        }
+
+
+        var min = int.MaxValue;
+        for(int i=0; i<3;i++)
+        {
+            if(n-pass[i]>=0)
+            {
+                min = Math.Min(dp[n-pass[i]] +costs[i], min  );       
+            }
+            else{
+                min = Math.Min(costs[i], min  );  
+            }
+        }
+        //Console.WriteLine($"day{n} with {min}");
+        dp[n] = min;
+    }
+    Console.WriteLine(string.Join(",",dp));      
+
+
+     return dp[dp.Count()-1];
+     
+    }
+}
