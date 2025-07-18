f8904ae5f0a5c5f50f35538ce52d469d84fa2ab0
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-07-18 06:48:33 -0400

    Time: 1861 ms (5.05%) | Memory: 62.8 MB (5.05%) - LeetSync

--- 55-jump-game/jump-game.cs
+++ 55-jump-game/jump-game.cs
@@ -1,12 +1,25 @@
 public class Solution {
-    public bool CanJump(int[] nums) {
-        var maxReach = 0;
-        for(int i =0 ; i < nums.Length;i++)
-        {
-            if(i>maxReach) return false;
+    public bool CanJump(int[] nums)
+    {
+        if(nums.Count() <=1 ) return true;
+        var ind =0;
 
-            maxReach = Math.Max(maxReach, i+nums[i]);
-            if(maxReach>nums.Length-1) return true;
+        while(ind < nums.Count()-1)
+        {
+            var add = 0;
+            var max = 0;
+            if(nums[ind] == 0) return false;
+            for(int i = 1 ; i<= nums[ind]; i++)
+            {
+                if(ind+i >= nums.Count()) return true;
+                if(max< ind + i + nums[ind+i])
+                {
+                    add = i;
+                    max = ind + i + nums[ind+i];
+                }
+            }
+            //Console.WriteLine($"ind {ind} +  add{add}  =  {ind+add}" );
+            ind+= add;
         }
         return true;
     }
