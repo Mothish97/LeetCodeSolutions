38a63e62a78fadbb265731a3b7ef291c6bf85ec6
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-07-19 08:32:13 -0400

    Time: 116 ms (6.13%) | Memory: 65.7 MB (6.75%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) 
+    {
+
+        var ct5 =0;
+        var ct10 =0;
+        var ct20=0;
+        foreach(var b in bills)
+        {
+            if(b == 5)
+            {
+                ct5++;
+            }
+            else if(b ==10)
+            {
+                ct5--;
+                ct10++;
+            }
+            else
+            {
+                if(ct10>0)
+                {
+                    ct10--;
+                    ct5--;
+                }
+                else
+                {
+                    ct5--;
+                    ct5--;
+                    ct5--;
+                }
+                
+                ct20++;
+            }
+            Console.WriteLine($"ct5 {ct5} ct10 {ct10} ct20 {ct20}");
+            if(ct5 <0 || ct10<0 || ct20<0) return false;
+        }
+        return true;
+    }
+}
