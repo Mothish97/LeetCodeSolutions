62f01451295a949131b88e233d24b283d52a6b5d
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-07-19 06:23:26 -0400

    Time: 13 ms (14.98%) | Memory: 42.8 MB (36.23%) - LeetSync

--- 649-dota2-senate/dota2-senate.cs
+++ 649-dota2-senate/dota2-senate.cs
@@ -3,11 +3,9 @@ public class Solution {
     {
         var dq = new Queue<int>();
         var rq = new Queue<int>();
-        var blocked = new HashSet<int>();
-        var indices = new List<int>();
-        for(int i =0 ; i< senate.Count(); i++)
+         int n = senate.Length;
+        for(int i =0 ; i< n; i++)
         {
-            indices.Add(i);
             if(senate[i] == 'D')
             {
                 dq.Enqueue(i);
@@ -16,24 +14,23 @@ public class Solution {
                 rq.Enqueue(i);
             }
         }
-        int n = senate.Length;
+       
         while(dq.Count() >0 && rq.Count()>0)
         {
             var d = dq.Dequeue();
             var r = rq.Dequeue();
             if(d<r)
             {
-                dq.Enqueue(d+n );
+                dq.Enqueue(d+n);
             }
             else{
-                rq.Enqueue(r+n );
+                rq.Enqueue(r+n);
             }
         }
 
 
         if(rq.Count() == 0) return "Dire";
-        if(dq.Count()==0) return "Radiant";
-        return "";
+        return "Radiant";
         
     }
 }
