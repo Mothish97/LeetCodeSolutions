505cc7be8b5649b056045d69e817054dfc23d8a8
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-07-31 03:30:33 -0400

    Time: 485 ms (0.00%) | Memory: 88.3 MB (0.00%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public long MaximumImportance(int n, int[][] roads) 
+    {
+        var freq = new Dictionary<int,int>();
+        var city = new Dictionary<int,int>();
+        for(int i =0;i<n;i++)
+        {
+            freq.Add(i,0);
+        }
+        foreach(var r in roads)
+        {
+            freq[r[0]]++;
+            freq[r[1]]++;
+        }
+
+        var sort = freq.OrderByDescending(pair => pair.Value);
+        var cur = n;
+        foreach(var d in sort)
+        {
+            Console.WriteLine($"for {d.Key} with freq {d.Value} added {n}");
+            city.Add(d.Key,n--);
+            if(n <0) n =0;
+        }
+        long res =0;
+
+        foreach(var r in roads)
+        {
+            res+= (long) city[r[0]] + (long)city[r[1]];
+        }
+        return res;
+    }
+}
