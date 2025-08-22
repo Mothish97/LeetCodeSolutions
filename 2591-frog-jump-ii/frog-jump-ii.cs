64babe97b96bb26b4321d83b235d7c70e269bd96
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-08-22 01:46:45 -0400

    Time: 12 ms (0.00%) | Memory: 63.5 MB (42.86%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.cs
+++ 2591-frog-jump-ii/frog-jump-ii.cs
@@ -1,14 +1,27 @@
 public class Solution 
 {
-    public int MaxJump(int[] stones) 
-    {
-        if (stones.Length == 2)
-            return stones[1] - stones[0];
-        int max = int.MinValue;
-        for (int i = 0; i < stones.Length - 2; i++)
-        {
-            max = Math.Max(max, stones[i + 2] - stones[i]);
-        }
-        return max;
+public int MaxJump(int[] stones) {
+    if (stones.Length is 2)
+        return stones[1];
+
+    int left = 1;
+    int right = stones[^1];
+    while (left < right) {
+        var mid = (right - left) / 2 + left;
+        if (Check(mid))
+            right = mid;
+        else
+            left = mid + 1;
     }
+    return left;
+
+    bool Check(int maxJump) {
+        for (int i = 2; i < stones.Length; i++) {
+            if (stones[i] - stones[i - 2] > maxJump)
+                // stone out of reach, maxJump is too short
+                return false;
+        }
+        return true;
+    }
+}
 }
