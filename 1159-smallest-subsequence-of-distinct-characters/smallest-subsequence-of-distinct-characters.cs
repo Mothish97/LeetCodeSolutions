4a828ed14c3b8e2c92caa069f1ec2061183ada9d
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-08-26 06:26:44 -0400

    Time: 174 ms (7.69%) | Memory: 41.5 MB (7.69%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public string SmallestSubsequence(string s) 
+    {
+        var dct = new Dictionary<char,int>();
+        for(int i =0; i<s.Count();i++)
+        {
+            if(!dct.ContainsKey(s[i]))
+            {
+                dct.Add(s[i],i);
+            }
+            dct[s[i]] = i;
+        }
+
+        var stck = new Stack<char>();
+        var inStack = new HashSet<char>();
+        for(int i=0;i<s.Count();i++)
+        {
+            if (inStack.Contains(s[i])) continue; 
+
+            while(stck.Count()>0 && stck.Peek() > s[i] && i< dct[stck.Peek()] )
+            {
+                inStack.Remove(stck.Pop());
+            }
+            stck.Push((s[i]));
+            inStack.Add(s[i]);
+            
+            
+        }
+        string distinct = new string(stck.Reverse().Select(x => x).Distinct().ToArray());
+        //Console.WriteLine(distinct);
+        return distinct;
+    }
+
+ 
+}
