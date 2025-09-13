ee53050387a94fea00917dca252da407cfc52abd
Mothish97 <86384730+Mothish97@users.noreply.github.com>
2025-09-13 15:58:26 -0400

    Time: 120 ms (22.22%) | Memory: 67.4 MB (22.22%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int MinimumSize(int[] nums, int maxOperations) 
+    {
+        Array.Sort(nums);
+        var left =1;
+        var right = nums.Max();
+        while(left <= right)
+        {
+            var mid = left + (right -left)/2;
+            if(IsPossible(nums,mid,maxOperations))
+            {
+                right = mid -1;
+            }
+            else
+            {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+    public bool IsPossible(int [] nums, int mid, int mx)
+    {
+        var cur = 0;
+        foreach(var n in nums)
+        {
+            var val = n;
+            if(val <= mid) continue;
+            cur += (val-1) /mid;
+            //cur--;
+            if(cur> mx) return false;
+            
+        }
+        return true;
+    }
+}
